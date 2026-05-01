734c8c04648359fa9e00ff694eafe3703a9b6fa1
Heitor <franciscoheitor986@gmail.com>
2026-05-01 11:22:56 -0300

    Ensure StartFit backend connects to Mongo on requests

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectToDatabase = require('./db');
 
 dotenv.config();
 
@@ -38,6 +39,15 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(async (req, res, next) => {
+  try {
+    await connectToDatabase();
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin');
